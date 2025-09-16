1878e8ee753f0b27e98d6050fa3c293ea5c3dc06
Chanwu Gim <1stpkloser@naver.com>
2025-09-16 13:26:25 +0900

    GAS APP URL 변경

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ 설정 영역 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
     // 
     // 1. Google Apps Script(GAS) 배포 후 생성된 웹 앱 URL을 여기에 붙여넣으세요.
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzvgnRgLJji65l9JSPGxkkoRDyhtu9Why4CK6pgc2k1y_s_jc5ewdszSn332rUspWaYOw/exec'; 
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyuqXPUff55dD1jf7P4BNrZzPqQYtzFocLXriyDvhhQHnnRa7pOaIIVRoZrCd-65ovrig/exec'; 
     //
     // 2. GAS 코드에 설정한 API 키와 동일한 값을 입력하세요.
     const API_KEY = 'GEM-PROJECT-GPH-2025';
